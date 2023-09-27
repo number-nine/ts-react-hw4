c41867d3d7939b33344c4d2b0b306e689b118496
number-nine <sergii.pokhylko@gmail.com>
2023-09-27 16:54:59 +0300

    1-4

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useRef } from 'react';
 
 // Опишіть Props
+
+type Props = {
+  onContentEndVisible(): void;
+  children: React.ReactNode;
+}
+
+type IntersectionOptions = {
+    rootMargin: string,
+      threshold: number,
+      root: Element | Document | null,
+}
+
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    const options:IntersectionOptions = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
-    };
+    }; //навіщо згадується клас в завданні?
 
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
